dc8c85b172c14d0aa4d623ae8b150a23ebc54784
edgarsilva <edgar.osc@gmail.com>
2015-01-28 20:42:48 -0600

    WIP: working on examples.

---
+++ examples/blink_led/blink-server.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var Cylon = require("cylon");
+
+var bots = {
+  "rosie": "",
+};
+
+Object.keys(bots).forEach(function(name) {
+  Cylon.robot({
+    name: name,
+
+    connections: {
+      arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
+    },
+
+    devices: {
+      asensor: { driver: 'analog-sensor', pin: 0 }
+    },
+
+    work: function() {
+      // Add your robot code here,
+      // for this example with sockets
+      // we are going to be interacting
+      // with the robot using the code in
+      // ./analog-read-client.html.
+
+    }
+  });
+});
+
+// ensure you install the API plugin first:
+// $ npm install cylon-api-socket-io
+Cylon.api(
+  'socket-io',
+  {
+  host: "0.0.0.0",
+  port: "3000"
+});
+
+Cylon.start();
