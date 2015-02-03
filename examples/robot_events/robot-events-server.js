6e7a9f077214a592cd8d0d27d9281935b2a1875a
edgarsilva <edgar.osc@gmail.com>
2015-02-03 15:17:54 -0600

    Added new robot-events example.

---
+++ examples/robot_events/robot-events-server.js
@@ -0,0 +1,64 @@
+"use strict";
+
+var Cylon = require("cylon");
+
+Cylon.robot({
+  name: 'cybot',
+
+  // These are the events that will be registered in the API
+  events: ['turned-on', 'turned-off'],
+
+  // These are the commands that will be availble in the API
+  // Commands method needs to return an object with the aliases
+  // to the robot methods.
+  commands: function() {
+    return {
+      turn_on: this.turnOn,
+      turn_off: this.turnOff,
+    };
+  },
+
+  connections: {
+    arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
+  },
+
+  devices: {
+    led: { driver: 'led', pin: 13 }
+  },
+
+  work: function() {
+    // Add your robot code here,
+    // for this example with sockets
+    // we are going to be interacting
+    // with the robot using the code in
+    // ./analog-read-client.html.
+    after((2).seconds(), function() {
+      this.turnOn();
+    }.bind(this));
+
+    after((5).seconds(), function() {
+      this.turnOff();
+    }.bind(this));
+  },
+
+  turnOn: function() {
+    this.led.turnOn();
+    this.emit('turned-on');
+  },
+
+  turnOff: function() {
+    this.led.turnOff();
+    this.emit('turned-off');
+  }
+});
+
+// ensure you install the API plugin first:
+// $ npm install cylon-api-socket-io
+Cylon.api(
+  'socketio',
+  {
+  host: "0.0.0.0",
+  port: "3000"
+});
+
+Cylon.start();
