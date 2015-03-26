3b1303e893f7b7ee85580fae8d2ae4a3189ec85d
deadprogram <ron@hybridgroup.com>
2015-03-25 17:26:19 -0700

    Simple example

---
+++ examples/simple_device/simple-server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var Cylon = require('cylon');
+
+Cylon.robot({
+  name: 'rosie',
+
+  connections: {
+    arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
+  },
+
+  devices: {
+    led: { driver: 'led', pin: 13 }
+  },
+
+  work: function() {
+    // for this example with sockets
+    // we are going to be interacting
+    // with the robot using the code in
+    // ./**-client.html
+  }
+});
+
+// ensure you install the API plugin first:
+// $ npm install cylon-api-socket-io
+Cylon.api(
+  'socketio',{
+  host: '0.0.0.0',
+  port: '3000'
+});
+
+Cylon.start();
